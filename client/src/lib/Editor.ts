ae9f935896e227752e5a9c38ad6c2d11c75d9cf9
vidigi <victor@voe.dk>
2025-02-24 16:09:46 +0100

    frontend afff

--- client/src/lib/Editor.ts
+++ client/src/lib/Editor.ts
@@ -1,27 +1,38 @@
-import type * as Monaco from "monaco-editor/esm/vs/editor/editor.api";
-import { Pathfinding } from "./user_modules/Pathfinding.ts";
-
+import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import { Pathfinding } from './user_modules/Pathfinding';
 
 let editor: Monaco.editor.IStandaloneCodeEditor;
 let monaco: typeof Monaco;
 
 export async function setupEditor(el: HTMLElement) {
-	monaco = (await import("./monaco.ts")).default;
+	console.log('setting up editor');
+
+	monaco = (await import('./monaco')).default;
+	el.innerHTML = ''; // Clear the container
 	editor = monaco.editor.create(el, {
 		automaticLayout: true,
-		theme: "vs",
-		wrappingStrategy: "advanced",
-		wordWrap: "on",
-		value: `// Welcome to the editor!`,
+		theme: 'vs',
+		wrappingStrategy: 'advanced',
+		wordWrap: 'on',
+		minimap: { enabled: false }, // Disable minimap
+		stickyScroll: { enabled: false }, // Disable sticky scroll
+		lineNumbers: 'off' // Show line numbers
 	});
-	const model = monaco.editor.createModel("", "typescript");
+	const model = monaco.editor.createModel(DEFAULT_VAL, 'typescript');
 	/* 	editor.onDidChangeModelContent(() => {
 		body = editor.getValue();
 	}); */
 	//maybe custom theme here maybe ?
 	editor.setModel(model);
-	
-	//expose Pathfinding module to the window object
-	(window as any).Pathfinding = Pathfinding;
+
+	console.log('editor setup complete', editor);
 	return editor;
 }
+
+export const DEFAULT_VAL = `// This is a comment
+function hello(name: string) {
+	console.log('Hello, ' + name);
+}
+
+hello('World');
+`;
