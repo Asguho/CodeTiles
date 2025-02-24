369973339c1e61958938d2265926b3c0ee04f7ee
Victor Digital <victordj587@gmail.com>
2025-02-24 14:05:57 +0100

    Add signup form to login page

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -1,4 +1,12 @@
+<h1>login</h1>
 <form action="http://localhost:8000/api/auth/login" method="post">
+	<input type="text" name="username" placeholder="Username" />
+	<input type="password" name="password" placeholder="Password" />
+	<button type="submit">Login</button>
+</form>
+
+<h1>signup</h1>
+<form action="http://localhost:8000/api/auth/signup" method="post">
 	<input type="text" name="username" placeholder="Username" />
 	<input type="password" name="password" placeholder="Password" />
 	<button type="submit">Login</button>
