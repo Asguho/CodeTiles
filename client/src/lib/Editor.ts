3408a168ce3b3ecc3869e5e619daa0d7362063f4
Victor Digital <victordj587@gmail.com>
2025-03-05 10:06:00 +0100

    get code

--- client/src/lib/Editor.ts
+++ client/src/lib/Editor.ts
@@ -1,58 +1,66 @@
-import type * as Monaco from "monaco-editor/esm/vs/editor/editor.api";
-import { Pathfinding } from "./user_modules/Pathfinding";
+import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import { Pathfinding } from './user_modules/Pathfinding';
 
 let editor: Monaco.editor.IStandaloneCodeEditor;
 let monaco: typeof Monaco;
 
 const theme: Monaco.editor.IStandaloneThemeData = {
-	base: "vs-dark" as Monaco.editor.BuiltinTheme,
+	base: 'vs-dark' as Monaco.editor.BuiltinTheme,
 	inherit: true,
 	rules: [
-		{ token: "comment", foreground: "76db6a" },
-		{ token: "keyword", foreground: "71b8ff" },
-		{ token: "string", foreground: "ffc065" },
-		{ token: "bracket", foreground: "71b8ff" },
+		{ token: 'comment', foreground: '76db6a' },
+		{ token: 'keyword', foreground: '71b8ff' },
+		{ token: 'string', foreground: 'ffc065' },
+		{ token: 'bracket', foreground: '71b8ff' }
 	],
 	colors: {
-		"editor.foreground": "#FFFFFF",
-		"editor.background": "#18181b",
-		"editorCursor.foreground": "#528fff",
-		"editorLineNumber.foreground": "#FFFFFF",
+		'editor.foreground': '#FFFFFF',
+		'editor.background': '#18181b',
+		'editorCursor.foreground': '#528fff',
+		'editorLineNumber.foreground': '#FFFFFF'
 		/* 'editor.selectionBackground': '#88000030' */
-	},
+	}
 };
 
 export async function setupEditor(el: HTMLElement) {
-	console.log("setting up editor");
+	console.log('setting up editor');
 
-	monaco = (await import("./monaco")).default;
+	monaco = (await import('./monaco')).default;
 
-	monaco.editor.defineTheme("myTheme", theme);
+	monaco.editor.defineTheme('myTheme', theme);
 
-	el.innerHTML = ""; // Clear the container
+	el.innerHTML = ''; // Clear the container
 	editor = monaco.editor.create(el, {
 		automaticLayout: true,
-		theme: "myTheme",
-		fontFamily: "JetBrains Mono",
+		theme: 'myTheme',
+		fontFamily: 'JetBrains Mono',
 		fontLigatures: true,
-		wrappingStrategy: "advanced",
-		wordWrap: "on",
+		wrappingStrategy: 'advanced',
+		wordWrap: 'on',
 		minimap: { enabled: false }, // Disable minimap
 		stickyScroll: { enabled: false }, // Disable sticky scroll
-		lineNumbers: "off", // Show line numbers
+		lineNumbers: 'off' // Show line numbers
 	});
-	const model = monaco.editor.createModel(DEFAULT_VAL, "typescript");
+	const model = monaco.editor.createModel(DEFAULT_VAL, 'typescript');
 	/* 	editor.onDidChangeModelContent(() => {
 		body = editor.getValue();
 	}); */
 	//maybe custom theme here maybe ?
 	editor.setModel(model);
 
-	console.log("editor setup complete", editor);
+	console.log('editor setup complete', editor);
+
+	//load code from server
+	let res = await fetch('/api/get_code', { method: 'GET', credentials: 'include' });
+	if (res.ok) {
+		let { code } = await res.json();
+		editor.setValue(code);
+	}
+
 	return editor;
 }
 
-export const DEFAULT_VAL = /*html*/ `
+export const DEFAULT_VAL = /*js*/ `
 Deno.serve(async (req) => {
 	console.stdlog = console.log.bind(console);
 	console.logs = [];
