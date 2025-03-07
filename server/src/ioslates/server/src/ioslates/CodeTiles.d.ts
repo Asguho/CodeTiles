21ab961dbdfc4a1e4c8c8a485c1d1738d88d6da8
Asguho <78410939+Asguho@users.noreply.github.com>
2025-03-07 14:36:48 +0100

    Create CodeTiles.d.ts

---
+++ server/src/ioslates/server/src/ioslates/CodeTiles.d.ts
@@ -0,0 +1,67 @@
+type tileType = 'unknown' | 'ground' | 'wall' | 'ore';
+type unitType = 'melee' | 'ranged' | 'miner';
+type direction = 'north' | 'south' | 'east' | 'west';
+declare class unit {
+    id: string;
+    health: number;
+    type: unitType;
+    position: {
+        x: number;
+        y: number;
+    };
+    protected game?: CodeTiles;
+    constructor(id: string, health: number, type: unitType, position: {
+        x: number;
+        y: number;
+    }, game?: CodeTiles);
+    move(direction: direction): void;
+}
+declare class tile {
+    type: tileType;
+    position: {
+        x: number;
+        y: number;
+    };
+    constructor(type: tileType, position: {
+        x: number;
+        y: number;
+    });
+}
+declare class base {
+    id: string;
+    owner: string;
+    position: {
+        x: number;
+        y: number;
+    };
+    health: number;
+    constructor(id: string, owner: string, position: {
+        x: number;
+        y: number;
+    }, health: number);
+}
+declare class shop {
+    game?: CodeTiles;
+    constructor(game?: CodeTiles);
+    buy(item: unitType, quantity: number): void;
+}
+export declare class CodeTiles {
+    #private;
+    readonly map: tile[][];
+    readonly units: unit[];
+    readonly bases: base[];
+    readonly coins: number;
+    readonly turn: number;
+    readonly shop: shop;
+    constructor(gameState: any);
+    addAction(action: any): void;
+    hookConsole(): void;
+    toJSON(): {
+        actions: {
+            units: any[];
+            shop: any[];
+        };
+        logs: any[];
+    };
+}
+export {};
