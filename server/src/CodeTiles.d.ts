da46c9596530dfc8a4296aab0ee13d283cc0449c
Asguho <78410939+Asguho@users.noreply.github.com>
2025-03-10 12:44:10 +0100

    Add TypeScript type definitions and update Deno configuration

--- server/src/CodeTiles.d.ts
+++ server/src/CodeTiles.d.ts
@@ -1,80 +1,70 @@
-type tileType = "unknown" | "ground" | "wall" | "ore";
-type unitType = "melee" | "ranged" | "miner";
-type direction = "north" | "south" | "east" | "west";
-declare class unit {
-	id: string;
-	health: number;
-	type: unitType;
-	position: {
-		x: number;
-		y: number;
-	};
-	protected game?: CodeTiles;
-	constructor(
-		id: string,
-		health: number,
-		type: unitType,
-		position: {
-			x: number;
-			y: number;
-		},
-		game?: CodeTiles
-	);
-	move(direction: direction): void;
-}
-declare class tile {
-	type: tileType;
-	position: {
-		x: number;
-		y: number;
-	};
-	constructor(
-		type: tileType,
-		position: {
-			x: number;
-			y: number;
-		}
-	);
-}
-declare class base {
-	id: string;
-	owner: string;
-	position: {
-		x: number;
-		y: number;
-	};
-	health: number;
-	constructor(
-		id: string,
-		owner: string,
-		position: {
-			x: number;
-			y: number;
-		},
-		health: number
-	);
-}
-declare class shop {
-	game?: CodeTiles;
-	constructor(game?: CodeTiles);
-	buy(item: unitType, quantity: number): void;
-}
-declare class CodeTiles {
-	#private;
-	readonly map: tile[][];
-	readonly units: unit[];
-	readonly bases: base[];
-	readonly coins: number;
-	readonly turn: number;
-	readonly shop: shop;
-	constructor(gameState: any);
-	addAction(action: any): void;
-	hookConsole(): void;
-	toJSON(): {
-		actions: {
-			units: any[];
-			shop: any[];
-		};
-		logs: any[];
-	};
+declare module "CodeTiles" {
+    type tileType = 'unknown' | 'ground' | 'wall' | 'ore' | 'base';
+    type unitType = 'melee' | 'ranged' | 'miner';
+    type direction = 'north' | 'south' | 'east' | 'west';
+    class unit {
+        id: string;
+        health: number;
+        owner: string;
+        type: unitType;
+        position: {
+            x: number;
+            y: number;
+        };
+        protected game?: CodeTiles;
+        constructor(id: string, health: number, owner: string, type: unitType, position: {
+            x: number;
+            y: number;
+        }, game?: CodeTiles);
+        move(direction: direction): void;
+    }
+    class tile {
+        type: tileType;
+        position: {
+            x: number;
+            y: number;
+        };
+        constructor(type: tileType, position: {
+            x: number;
+            y: number;
+        });
+    }
+    class base extends tile {
+        owner: string;
+        health: number;
+        constructor(owner: string, position: {
+            x: number;
+            y: number;
+        }, health: number);
+    }
+    class shop {
+        game?: CodeTiles;
+        constructor(game?: CodeTiles);
+        buy(item: unitType, quantity: number): void;
+    }
+    class Game {
+        readonly playerId: string;
+        readonly map: tile[][];
+        readonly units: unit[];
+        readonly base: base;
+        readonly coins: number;
+        readonly turn: number;
+        readonly shop: shop;
+        constructor(gameState: any, codeTiles: CodeTiles);
+    }
+    export class CodeTiles {
+        #private;
+        constructor(gameState: any);
+        onTurn(f: (game: Game) => void): void;
+        evaluate(): void;
+        addAction(action: any): void;
+        toJSON(): {
+            actions: {
+                units: any[];
+                shop: any[];
+            };
+            logs: any[];
+        };
+        hookConsole(): void;
+    }
 }
