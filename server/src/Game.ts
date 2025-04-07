ab887eac3648a8b54f734611f90245776f6568f8
Asguho <78410939+Asguho@users.noreply.github.com>
2025-04-07 09:42:05 +0200

    Update Game.ts

--- server/src/Game.ts
+++ server/src/Game.ts
@@ -27,7 +27,7 @@ export class Game {
     private cleanUp: (outCome: string[] | null) => void,
     private callback?: (game: Game) => void,
   ) {
-    this.players = players.map(({ id, url }) => ({
+    this.players = [...players].sort(() => Math.random() - 0.5).map(({ id, url }) => ({
       id,
       serverUrl: url,
       units: [],
