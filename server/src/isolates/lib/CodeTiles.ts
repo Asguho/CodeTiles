a785b1450129f053ca9c60bebb79c90e693925d5
Asguho <78410939+Asguho@users.noreply.github.com>
2025-04-10 09:52:54 +0200

    Update CodeTiles.ts

--- server/src/isolates/lib/CodeTiles.ts
+++ server/src/isolates/lib/CodeTiles.ts
@@ -55,10 +55,7 @@ class Unit {
       if (x < 0 || y < 0 || y >= this.game.map.tiles.length || x >= this.game.map.tiles[0].length) {
         return false;
       }
-      if (this.game.map.tiles[y][x].type !== "wall") {
-        return true;
-      }
-      return !this.game.units.some((unit) => unit.position.x === x && unit.position.y === y);
+      return this.game.map.tiles[y][x].type !== "wall";
     };
     const path = Pathfinding.findPath(this.position, target, isWalkable);
     if (path && path.length > 0) {
